5b0c81a65f58351fa6ac39094261cabfb0eb892c
Quinn Keast <quinn@airplane.dev>
2023-03-29 10:17:58 -0700

    Laying tracks

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
+import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title key="title">Story Beats</title>
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
