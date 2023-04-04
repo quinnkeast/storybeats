803f6e383beba051beb307fc175d5dd2787c3123
Quinn Keast <quinn@airplane.dev>
2023-04-04 18:38:39 +0200

    bad test

--- pages/api/beats.js
+++ pages/api/beats.js
@@ -39,6 +39,6 @@ export default async function handler(req, res) {
     return res.status(200).json(beats);
   } catch (error) {
     console.error(error);
-    return res.status(400).json(error);
+    return res.status(200).json(error);
   }
 }
